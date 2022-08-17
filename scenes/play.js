22e965b34823c5f0a1a7ae221452c731737b5529
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2022-08-17 02:45:42 +0200

    direcciones

--- scenes/play.js
+++ scenes/play.js
@@ -8,7 +8,8 @@ class Play extends Phaser.Scene {
         this.CONFIG = this.sys.game.CONFIG;
         this.DEPTH = {
             floor: 0,
-            player: 1
+            player: 1,
+            ui: 2
         };
 
         this.generator = new Generator(this);
@@ -17,6 +18,13 @@ class Play extends Phaser.Scene {
         this.allow_input = false;
         this.is_pause = false;
         this.is_gameover = false;
+
+        // Controls
+        this.is_holding = {
+            left: false,
+            right: false,
+            direction: false
+        };
 
         // Camera
         this.cam_speed = {
@@ -32,6 +40,14 @@ class Play extends Phaser.Scene {
 
         // Player
         this.createPlayer();
+
+        // Controls
+        this.createControls();
+
+        // Start the game
+        this.allow_input = true;
+        this.is_pause = false;
+        this.is_gameover = false;
     }
 
     update() {
@@ -44,6 +60,13 @@ class Play extends Phaser.Scene {
 
         // Move player downward
         this.player.setSpritePosition(this.player.x, this.player.y + this.cam_speed.current);
+
+        // Move player sideways
+        if (this.is_holding.direction === 'left') {
+            this.player.setSpritePosition(this.player.x - 1, this.player.y);
+        } else if (this.is_holding.direction === 'right') {
+            this.player.setSpritePosition(this.player.x + 1, this.player.y);
+        }
 
 
     }
@@ -84,8 +107,76 @@ class Play extends Phaser.Scene {
             this.cam_speed.current,
             0
         );
+    }
 
+    // Controls ...............................
+    createControls() {
+        // Create zones for inputs
+        let w = 0.45 * this.CONFIG.width;
+        let h = this.CONFIG.height;
 
+        this.zone_left = this.add.zone(0, 0, w, h);
+        this.zone_left.setOrigin(0, 0);
+        this.zone_left.setDepth(this.DEPTH.ui);
+        this.zone_left.setScrollFactor(0);
+
+        this.zone_right = this.add.zone(this.CONFIG.width, 0, w, h);
+        this.zone_right.setOrigin(1, 0);
+        this.zone_right.setDepth(this.DEPTH.ui);
+        this.zone_right.setScrollFactor(0);
+
+        // let debug = this.add.graphics({ x: 0, y: 0 });
+        // debug.fillStyle('0x000000', 0.5);
+        // debug.fillRect(0, 0, w, h);
+        // debug.setScrollFactor(0);
+        // debug.setDepth(this.DEPTH.ui);
+
+        //Add input callbacks
+        this.zone_left.setInteractive();
+        this.zone_left.on('pointerdown', this.holdLeft, this);
+        this.zone_left.on('pointerup', this.releaseLeft, this);
+        this.zone_left.on('pointerout', this.releaseLeft, this);
+
+        this.zone_right.setInteractive();
+        this.zone_right.on('pointerdown', this.holdRight, this);
+        this.zone_right.on('pointerup', this.releaseRight, this);
+        this.zone_right.on('pointerout', this.releaseRight, this);
+    }
+
+    holdLeft() {
+        if (!this.allow_input) return;
+        if (this.is_pause || this.is_gameover) return;
+
+        this.is_holding.left = true;
+        this.is_holding.direction = 'left';
+    }
+
+    holdRight() {
+        if (!this.allow_input) return;
+        if (this.is_pause || this.is_gameover) return;
+
+        this.is_holding.right = true;
+        this.is_holding.direction = 'right';
+    }
+
+    releaseLeft() {
+        this.is_holding.left = false;
+
+        if (this.is_holding.right) {
+            this.is_holding.direction = 'right';
+        } else {
+            this.is_holding.direction = false;
+        }
+    }
+
+    releaseRight() {
+        this.is_holding.right = false;
+
+        if (this.is_holding.left) {
+            this.is_holding.direction = 'left';
+        } else {
+            this.is_holding.direction = false;
+        }
     }
 
 }
